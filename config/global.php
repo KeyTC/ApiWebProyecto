f820b9cfb3d6e7fdef5cb65cb0cfe078ea616375
keylor <91291890+KeyTC@users.noreply.github.com>
2023-11-24 12:17:40 -0600

    Update global.php

--- config/global.php
+++ config/global.php
@@ -1,15 +1,17 @@
 <?php 
 //Ip de la pc servidor de base de datos
-define("DB_HOST","localhost");
+define("DB_HOST",DB_HOST);
 
 //Nombre de la base de datos
-define("DB_NAME", "bdescuela");
+define("DB_NAME", DB_NAME);
 
 //Usuario de la base de datos
-define("DB_USERNAME", "root");
+define("DB_USERNAME", DB_USERNAME);
 
 //Contraseña del usuario de la base de datos
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", DB_PASSWORD);
+
+define("DB_PORT", DB_PORT);
 
 //definimos la codificación de los caracteres
 define("DB_ENCODE","utf8");
